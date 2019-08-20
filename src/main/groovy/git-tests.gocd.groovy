2d292748b42a7fb85b53c739b9789d6cf01c4fae
Akshay Dewan <akshayde@thoughtworks.com>
2019-08-20 14:39:49 +0530

    Add git 2.23.0

--- src/main/groovy/git-tests.gocd.groovy
+++ src/main/groovy/git-tests.gocd.groovy
@@ -2,7 +2,7 @@ import cd.go.contrib.plugins.configrepo.groovy.dsl.GitMaterial
 import cd.go.contrib.plugins.configrepo.groovy.dsl.GoCD
 import cd.go.contrib.plugins.configrepo.groovy.dsl.Job
 
-final def versions = ["2.22.0", "2.21.0", "2.20.1", "2.19.2", "2.18.1", "2.17.2", "2.16.5", "2.15.3", "2.14.5", "2.13.7", "2.12.5", "2.11.4",
+final def versions = ["2.23.0", "2.22.0", "2.21.0", "2.20.1", "2.19.2", "2.18.1", "2.17.2", "2.16.5", "2.15.3", "2.14.5", "2.13.7", "2.12.5", "2.11.4",
                       "2.10.5", "2.9.5", "2.8.6", "2.7.6", "2.6.7", "2.5.6", "2.4.9", "2.3.9", "2.2.3", "2.1.4", "2.0.5", "1.9.5"]
 
 def rpmMaterial = new GitMaterial("rpms", {
